96cd4fb0ec720e7bac25dd32dee47266dd392911
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-08 23:41:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+    let result = [];
+    for (i = 0; i < arr.length; i++) {
+
+    }
+}
+
+function countDown() {
+    let downCount = 11;
+    while (downCount > 0) {
+        downCount--;
+        console.log(downCount)
+    }
+}
